1457bffa9c2833847f91aeab9c0add5638569504
bob.burrough <bob.burrough@d3f1cc82-c306-40fe-8310-c3bf58b39332>
2016-07-08 18:25:00 +0000

    Change the interface of the file picker API such that it doesn't work as a singleton.  With this, multiple instsance of the file picker may be invoked at a time.

--- Natives/natives.h
+++ Natives/natives.h
@@ -13,13 +13,15 @@ extern "C"
 #define DLLEXPORT extern __declspec(dllimport)
 #endif
 
-    typedef void(__stdcall *ChooseFileSuccessCallback)();
-    DLLEXPORT void __stdcall RegisterChooseFileSuccessCallback(ChooseFileSuccessCallback callback);
+typedef void(__stdcall *ChooseFileSuccessCallback)(const wchar_t* str);
+typedef void(__stdcall *ChooseFileCancelledCallback)();
 
-    typedef void(__stdcall *ChooseFileCancelledCallback)();
-    DLLEXPORT void __stdcall RegisterChooseFileCancelledCallback(ChooseFileCancelledCallback callback);
+DLLEXPORT void* __stdcall CreateChooseFileContext();
+DLLEXPORT void __stdcall AddChooseFileType(void* context, const wchar_t* name, const wchar_t* extension);
+DLLEXPORT void __stdcall ChooseFile(void* context, ChooseFileSuccessCallback successCallback, ChooseFileCancelledCallback cancelledCallback);
+DLLEXPORT void __stdcall DestroyChooseFileContext(void* context);
 
-    DLLEXPORT void __stdcall ChooseFile();
+//DLLEXPORT void __stdcall Teardown();
 
 #ifdef __cplusplus
 }
